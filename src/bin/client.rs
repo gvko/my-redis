ff77fe368756fbd98ff3545c71c3a66900e0764a
Galin Kostov <dev@galin.cc>
2023-06-10 21:14:56 +0300

    Make the client into a CLI

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -2,42 +2,71 @@ use std::str::{from_utf8};
 use bytes::BytesMut;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Get {
+        key: String,
+    },
+    Set {
+        key: String,
+        value: String,
+    },
+}
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let args = Cli::parse();
     let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
 
-    stream.write_all(b"set foo bar").await?;
-    let mut buf = BytesMut::with_capacity(1024);
-    let _stream_length = stream.read_buf(&mut buf).await?;
+    match args.command {
+        Command::Get { key } => {
+            stream.write_all(b"get ").await?;
+            stream.write_all(key.as_bytes()).await?;
 
-    match from_utf8(&mut buf) {
-        Ok(res) => {
-            if res == "r Ok" {
-                println!("key updated");
-            } else if res == "Ok" {
-                println!("key set");
-            } else {
-                eprintln!("Unrecognized response: {}", res);
+            let mut buf = BytesMut::with_capacity(1024);
+            stream.read_buf(&mut buf).await?;
+
+            match from_utf8(&mut buf) {
+                Ok(res) => {
+                    if res == "" {
+                        println!("No key found");
+                    } else {
+                        println!("value: {}", res);
+                    }
+                }
+                Err(err) => eprintln!("Error: {}", err)
             }
         }
-        Err(err) => eprintln!("Error: {}", err)
-    }
+        Command::Set { key, value } => {
+            stream.write_all(b"set ").await?;
+            stream.write_all(key.as_bytes()).await?;
+            stream.write_all(b" ").await?;
+            stream.write_all(value.as_bytes()).await?;
 
-    let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
-    stream.write_all(b"get foo").await?;
-    let mut buf = BytesMut::with_capacity(1024);
-    stream.read_buf(&mut buf).await?;
+            let mut buf = BytesMut::with_capacity(1024);
+            stream.read_buf(&mut buf).await?;
 
-    match from_utf8(&mut buf) {
-        Ok(res) => {
-            if res == "" {
-                println!("No key found");
-            } else {
-                println!("value: {}", res);
+            match from_utf8(&mut buf) {
+                Ok(res) => {
+                    if res == "r Ok" {
+                        println!("key updated");
+                    } else if res == "Ok" {
+                        println!("key set");
+                    } else {
+                        eprintln!("Unrecognized response: {}", res);
+                    }
+                }
+                Err(err) => eprintln!("Error: {}", err)
             }
         }
-        Err(err) => eprintln!("Error: {}", err)
     }
 
     Ok(())
